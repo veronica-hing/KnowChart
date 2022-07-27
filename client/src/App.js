81e4719f145263395fceb530d8cd9377e5ecca29
veronica-hing <veronica.m.hing@gmail.com>
2022-07-26 20:18:15 -0700

    implemented drag and drop toolbar to create flowchart

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Flow from './components/Flow';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          KnowChart
-        </p>
-      </header>
+      {/* size specified here otherwise the flow doesnt render properly */}
+      <div style={{width:'90%', height:'60vh',border: 'solid 2px red'}}>
+        <Flow/>
+      </div>
     </div>
   );
 }
